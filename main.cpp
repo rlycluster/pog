05f6e8675763595f8afa3785718b32272daf7faa
Cluster <83150885+rlycluster@users.noreply.github.com>
2023-12-01 20:46:03 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  solve();
+
+  return 0;
+}
